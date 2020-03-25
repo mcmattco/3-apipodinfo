6c4859765d4e3175f94caf8ef34393e8f7ecabf5
Matt McClure <mattmcclure@matts-mbp.lan>
2020-03-25 11:35:47 -0400

    updated bash script

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+elb='elb.amazonaws.com'
+
+curloutput=$( \
 curl \
 -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
 --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
@@ -5,5 +10,11 @@ curl \
 -H "Accept: application/json" \
 -s \
 https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT/api/v1/namespaces/ingress-nginx/services/ingress-nginx \
-| jq .status.loadBalancer.ingress[].hostname \
-> /work-dir/stuff.html
+)
+
+if [[ "$curloutput" == *"$elb"* ]]
+then
+  echo $curloutput | jq .status.loadBalancer.ingress[].hostname > /work-dir/stuff.html
+else
+  echo $curloutput | jq .message > /work-dir/stuff.html
+fi
