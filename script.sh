a7f4ca88a87877b653bfb833d72639b0c5cdda24
Matt McClure <mattmcclure@matts-mbp.lan>
2020-03-30 14:18:41 -0400

    added serviceaccount

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 elb='elb.amazonaws.com'
+saname='something'
 
 curloutput=$( \
 curl \
@@ -12,9 +13,11 @@ curl \
 https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT/api/v1/namespaces/ingress-nginx/services/ingress-nginx \
 )
 
+echo -e "The service account running this pod is" $saname "<br>" > /work-dir/stuff.html
+
 if [[ "$curloutput" == *"$elb"* ]]
 then
-  echo $curloutput | jq .status.loadBalancer.ingress[].hostname > /work-dir/stuff.html
+  echo $curloutput | jq .status.loadBalancer.ingress[].hostname >> /work-dir/stuff.html
 else
-  echo $curloutput | jq .message > /work-dir/stuff.html
+  echo $curloutput | jq .message >> /work-dir/stuff.html
 fi
